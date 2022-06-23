a61dd2734a09bc64f007b8fd8c0792d96e210d32
Eddy Marcatoma Cardenas <eddyandres92@gmail.com>
2022-06-24 01:27:47 +0200

    Implemented HttpException
    Added error for wrong user or password in login
    Updated makeNetworkCall

--- app/src/main/java/com/example/perrodex/api/makeNetworkCall.kt
+++ app/src/main/java/com/example/perrodex/api/makeNetworkCall.kt
@@ -3,7 +3,10 @@ package com.example.perrodex.api
 import com.example.perrodex.R
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import retrofit2.HttpException
 import java.net.UnknownHostException
+
+private const val UNAUTHORIZED_ERROR_CODE = 401
 
 suspend fun <T> makeNetworkCall(
     call: suspend () -> T
@@ -12,6 +15,13 @@ suspend fun <T> makeNetworkCall(
         ApiResponseStatus.Success(call())
     } catch (u: UnknownHostException) {
         ApiResponseStatus.Error(R.string.unknown_host_exception_error)
+    } catch (h: HttpException) {
+        val errorMessage = if(h.code() == UNAUTHORIZED_ERROR_CODE){
+            R.string.wrong_user_or_password
+        } else {
+            R.string.unknown_error
+        }
+        ApiResponseStatus.Error(errorMessage)
     } catch (e: Exception) {
         val errorMessage = when (e.message) {
             "sign_up_error" -> R.string.error_sign_up
@@ -22,3 +32,4 @@ suspend fun <T> makeNetworkCall(
         ApiResponseStatus.Error(errorMessage)
     }
 }
+
