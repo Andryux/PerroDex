073f6fd9e40d25f8d0c1e1adf6049e050f9c5c23
Eddy Marcatoma Cardenas <eddyandres92@gmail.com>
2022-06-24 15:29:29 +0200

    Added interceptor to request
    Create ApiServiceInterceptor, AddDogToUserDTO, DefaultResponse
    Updated other classes

--- app/src/main/java/com/example/perrodex/api/ApiService.kt
+++ app/src/main/java/com/example/perrodex/api/ApiService.kt
@@ -1,30 +1,43 @@
 package com.example.perrodex.api
 
 import com.example.perrodex.*
+import com.example.perrodex.api.dto.AddDogToUserDTO
 import com.example.perrodex.api.dto.LogInDTO
 import com.example.perrodex.api.dto.SignUpDTO
 import com.example.perrodex.api.responses.DogListApiResponse
 import com.example.perrodex.api.responses.AuthApiResponse
+import com.example.perrodex.api.responses.DefaultResponse
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.POST
+
+private val okHttpClient = OkHttpClient
+    .Builder()
+    .addInterceptor(ApiServiceInterceptor)
+    .build()
 
 private val retrofit = Retrofit.Builder()
     .baseUrl(BASE_URL)
     .addConverterFactory(MoshiConverterFactory.create())
     .build()
 
-interface ApiService{
+interface ApiService {
     @GET(GET_ALL_DOGS_URL)
     suspend fun getAllDogs(): DogListApiResponse
+
+    @POST(SIGN_UP_URL)
+    suspend fun signUp(@Body signUpDTO: SignUpDTO): AuthApiResponse
 
     @POST(LOG_IN_URL)
     suspend fun logIn(@Body logInDTO: LogInDTO): AuthApiResponse
 
-    @POST(SIGN_UP_URL)
-    suspend fun signUp(@Body signUpDTO: SignUpDTO): AuthApiResponse
+    @Headers("${ApiServiceInterceptor.NEEDS_AUTH_HEADER_KEY}: true")
+    @POST(ADD_DOG_TO_USER_URL)
+    suspend fun addDogToUser(@Body addDogToUserDTO: AddDogToUserDTO): DefaultResponse
 }
 
 object DogApi {
---
+++ app/src/main/java/com/example/perrodex/api/dto/AddDogToUserDTO.kt
@@ -0,0 +1,7 @@
+package com.example.perrodex.api.dto
+
+import com.squareup.moshi.Json
+
+class AddDogToUserDTO (@field:Json(name = "dog_id") val dogId: String){
+
+}
