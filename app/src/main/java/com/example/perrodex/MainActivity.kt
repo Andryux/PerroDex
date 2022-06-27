feb5443b4d95b9379de0998b33587a9061f00817
Eddy Marcatoma Cardenas <eddyandres92@gmail.com>
2022-06-28 00:12:01 +0200

    Asking permissions to use camera

--- app/src/main/java/com/example/perrodex/MainActivity.kt
+++ app/src/main/java/com/example/perrodex/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.example.perrodex
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import com.example.perrodex.api.ApiServiceInterceptor
 import com.example.perrodex.auth.LoginActivity
 import com.example.perrodex.databinding.ActivityMainBinding
@@ -11,13 +18,29 @@ import com.example.perrodex.model.User
 import com.example.perrodex.settings.SettingsActivity
 
 class MainActivity : AppCompatActivity() {
+
+    // Register the permissions callback, which handles the user's response to the
+    // system permissions dialog. Save the return value, an instance of
+    // ActivityResultLauncher. You can use either a val, as shown in this snippet,
+    // or a lateinit var in your onAttach() or onCreate() method.
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                //Open camera
+            } else {
+                Toast.makeText(this, "You need to accept camera permission to use camera", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val user = User.getLoggedInUser(this)
-        if(user == null){
+        if (user == null) {
             openLoginActivity()
             return
         } else {
@@ -30,6 +53,45 @@ class MainActivity : AppCompatActivity() {
 
         binding.dogListFab.setOnClickListener {
             openDogListActivity()
+        }
+
+        requestCameraPermission()
+    }
+
+    private fun requestCameraPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                // You can use the API that requires the permission.
+            }
+            shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)
+            -> {
+                // In an educational UI, explain to the user why your app requires this
+                // permission for a specific feature to behave as expected. In this UI,
+                // include a "cancel" or "no thanks" button that allows the user to
+                // continue using your app without granting the permission.
+                AlertDialog.Builder(this)
+                    .setTitle("Aceptar Permisos")
+                    .setMessage("Los permisos de camara son necesarios")
+                    .setPositiveButton(android.R.string.ok) {
+                        _, _ ->
+                        requestPermissionLauncher.launch(
+                            Manifest.permission.CAMERA
+                        )
+                    }
+                    .setNegativeButton(android.R.string.cancel){
+                        _, _ ->
+                    }.show()
+            }
+            else -> {
+                // You can directly ask for the permission.
+                // The registered ActivityResultCallback gets the result of this request.
+                requestPermissionLauncher.launch(
+                    Manifest.permission.CAMERA
+                )
+            }
         }
     }
 
